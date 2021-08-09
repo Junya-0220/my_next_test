fb4d7762ee4c7a145336134cbbf2328feba0e4e0
Junya-0220 <54745459+Junya-0220@users.noreply.github.com>
2021-08-09 20:40:11 +0900

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  future: { strictPostcssConfiguration: true },
+  pageExtensions: ["page.tsx", "page.ts"],
+  i18n: { locales: ["ja"], defaultLocale: "ja" },
+  reactStrictMode: true,
+  typescript: { ignoreDevErrors: true },
+  poweredByHeader: false,
+};
